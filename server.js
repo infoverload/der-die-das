234fc41f64ed120f288d0c03441916cb92466106
infoverload <infoverload@users.noreply.github.com>
2016-10-24 16:48:24 +0200

    change server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var express = require('express');
 var serveStatic = require('serve-static');
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 app.use(serveStatic('./public'));
 
 app.get('*', function (req, res) {
-  res.send('This is not the page you where looking for!');
+  res.send('This is not the page you were looking for!');
 });
 
 app.listen(port, function () {
-  console.log('server listening in port', port);
+  console.log('Server listening in port: ', port);
 });
